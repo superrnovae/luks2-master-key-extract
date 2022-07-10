9f61ec8585afd9cf1197257e4c0778a310836ce6
parphelion <vladislavp@pm.me>
2022-07-10 18:31:25 +0200

    Add script to generate binary master keys and test them with cryptsetup

---
+++ script.py
@@ -0,0 +1,33 @@
+import binascii
+import os
+from subprocess import run
+
+device = '/dev/any/any'
+
+
+def generate():
+    with open('pairs') as f_in:
+        lines = f_in.readlines()
+        for i in range(1, len(lines), 2):
+            print(lines[i])
+            if i+1 < len(lines):
+                p1 = lines[i].rstrip().replace(" ", "")
+                p2 = lines[i+2].rstrip().replace(" ", "")
+                chunk = binascii.unhexlify(p1+p2)
+                with open(f"key{i}.bin", 'wb') as f_out:
+                    f_out.write(chunk)
+                    f_out.close()
+        f_in.close()
+
+
+def penetrate():
+    file_path = os.path.dirname(os.path.abspath(__file__))
+    for file in os.listdir(file_path):
+        p = run(['sudo', 'cryptsetup', 'luksAddKey', f'{device}', '--master-key-file', f'{file}'])
+        if p.returncode == 0:
+            exit()
+
+
+if __name__ == '__main__':
+    generate()
+    penetrate()
